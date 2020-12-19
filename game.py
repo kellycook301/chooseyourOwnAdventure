fdfa9555ffb8b2190d41406d690edfafacdc29b1
Kelly Cook <kellycook20878@gmail.com>
2020-12-19 11:16:38 -0600

    trying sound sample

--- game.py
+++ game.py
@@ -59,7 +59,7 @@ def confrontOrEscape(name):
     path = ""
     while path != "confront" and path != "escape":
         print("The bandits are coming up the stairs, " + name + ". You're going to have to fight them or make a break through the window.")
-        path = raw_input("Do you attempt to confront the enemy or escape? confront/escape \n").lower().strip()
+        path = raw_input("Do you attempt to confront the enemy or escape?? confront/escape \n").lower().strip()
 
     if path == "confront":
         print("")
@@ -288,7 +288,7 @@ def confrontOrSubmit(name):
         print("Every soldier scrambles getting up and out of their tents; drawing their swords ready to pounce on you and tear you limb from limb")
         print("A tall man appears from the largest tent.")
         print("'And who might you be?'")
-        print(storyIntro.character_Name + "you say.")
+        print("'It's " + name + ".'")
         print("You explain that you are the son of a farmer and that they have killed your father in cold blood.")
         print("'AH. The boy who escaped earlier. I remember. I remember.'")
         print("'You know...it takes a lot of courage to come here in the middle of the night and confront all of us.'")
@@ -336,84 +336,141 @@ def shoutOrSilent(name):
     while shout != "shout" and shout != "silent":
         shout = raw_input("He's in your sights. Do you attack silently or by screaming, hoping to throw him off guard? silent/shout \n").lower().strip()
 
-        if shout == "shout":
-            print("")
-            print("You get at a good distance and run towards him, screaming at the top of your lungs.")
-            print("The bandit is totally startled and goes for his short sword, but fumbles it out of fear.")
-            print("You smash the rock over his head. The bandit stumbles and falls backwards over a 30-foot cliff.")
-            print("You then make your way back to the cave to plan your next move.")
-            print("")
-            print("As you climb further and further down a thick green fog begins to form")
-            print("'Wow. I didn't think I was in the fucking JUNGLE. Wheere did this fog come from?' you wonder.")
-            print("You go through the fog and notice that there is a fork ahead of you.")
-            print("...like a divulging path. Not a literal fork...you get it.")
-            print("'Ah shit. One of these, huh?'")
-            print("")
-            leftRight(name)
-            print("")
+    if shout == "shout":
+        print("")
+        print("You get at a good distance and run towards him, screaming at the top of your lungs.")
+        print("The bandit is totally startled and goes for his short sword, but fumbles it out of fear.")
+        print("You smash the rock over his head. The bandit stumbles and falls backwards over a 30-foot cliff.")
+        print("You then make your way back to the cave to plan your next move.")
+        print("")
+        print("As you climb further and further down a thick green fog begins to form")
+        print("'Wow. I didn't think I was in the fucking JUNGLE. Wheere did this fog come from?' you wonder.")
+        print("You go through the fog and notice that there is a fork ahead of you.")
+        print("...like a divulging path. Not a literal fork...you get it.")
+        print("'Ah shit. One of these, huh?'")
+        print("")
+        leftRight(name)
+        print("")
 
-        if shout == "silent":
-            print("")
-            print("You start sneaking up behind the bandit and get within a good distance.")
-            print("You raise the rock over your head and prepare to smash his brains in.")
-            print("In an instant the bandit side-steps and takes a swipe at your mid-section with his short sword.")
-            print("You drop and the rock on your foot and fall to the ground.")
-            print("You bleed out and die a painful death.")
-            print("")
-            playAgain(name)
-            print("")        
+    if shout == "silent":
+        print("")
+        print("You start sneaking up behind the bandit and get within a good distance.")
+        print("You raise the rock over your head and prepare to smash his brains in.")
+        print("In an instant the bandit side-steps and takes a swipe at your mid-section with his short sword.")
+        print("You drop and the rock on your foot and fall to the ground.")
+        print("You bleed out and die a painful death.")
+        print("")
+        playAgain(name)
+        print("")        
 
 def leftRight(name):
     direction = ""
     while direction != "left" and direction != "right":
         direction = raw_input("Do you take the left path or the right path? left/right \n").lower().strip()
 
-        if direction == "left":
-            print("") 
-            print("You decide to go down the bigger cavern to the left. It's dark but at least you won't be insanely claustrophobic.")
-            print("You keep trekking down until you reach ther end of the tunnel. At then end is a large door about ten feet tall with torches on each side.")
-            turnBackOrGoIn(name)
-            print("")
+    if direction == "left":
+        print("") 
+        print("You decide to go down the bigger cavern to the left. It's dark but at least you won't be insanely claustrophobic.")
+        print("You keep trekking down until you reach ther end of the tunnel. At then end is a large door about ten feet tall with torches on each side.")
+        turnBackOrGoIn(name)
+        print("")
 
-        if direction == "right":
-            print("") 
-            print("A smaller cave is no problem to you. You decide to climb into the smaller hold head first.")
-            print("You inch forward every so slowly. It feels like forever, but you reach the source of the dim light.")
-            print("You crawl out of the hole and emerge into a large cave with a single bright torch.")
-            print("Beside the torch is a tall figure in a black cape that covers its entire body.")
-            youWentRight(name)
-            print("")
+    if direction == "right":
+        print("") 
+        print("A smaller cave is no problem to you. You decide to climb into the smaller hold head first.")
+        print("You inch forward every so slowly. It feels like forever, but you reach the source of the dim light.")
+        print("You crawl out of the hole and emerge into a large cave with a single bright torch.")
+        print("Beside the torch is a tall figure in a black cape that covers its entire body.")
+        print("Its decaying hand outstrecthes to yours.")
+        print(name + ", I seeeeee rightttt throuuuuugh yooooou. Yooooou havvvvve commmmmeeee herrrrre lookinnnnnnng forrrr answersssss.'")
+        print("'I haaaaave whatttttt yoooooou seeeeeeek.'")
+        print("Before you appears two swords.")
+        print("'Withhhhhhh thissssss youuuuu cannnnnnn takeeeeee baccccck whatttttt isssss yoursssss. Yourrrrrr faaaaaaaather.'")
+        youWentRight(name)
+        print("")
 
 def turnBackOrGoIn(name):
     door = ""
     while door != "go in" and door != "turn back":
         door = raw_input("Do you go through the door or turn back and go the other way? go in/turn back \n").lower().strip()
 
-        if door == "go in":
-            print("")
-            print("You enter the door and there is a crazy old witch lady.")
-            print("She has a riddle for you. If you can answer it correctly she will give you a sword to vanquish your foes with ease.")
-            print("If you fail, you will die.")
-            witchRiddle(name)
-            print("")
+    if door == "go in":
+        print("")
+        print("You enter the door and there is a crazy old witch lady.")
+        print("She has a riddle for you. If you can answer it correctly she will give you a sword to vanquish your foes with ease.")
+        print("If you fail, you will die.")
+        witchRiddle(name)
+        print("")
 
-        if door == "turn back":
-            print("")
-            print("You decide to turn around and not see what craziness awaits you on the other side of the door")
-            print("You make your way back up the fork in the cave and go down the right side instead.")
-            print("A smaller cave is no problem to you. Could be better than the creepy door. You decide to climb into the smaller hold head first.")
-            print("You inch forward every so slowly. It feels like forever, but you reach the source of the dim light.")
-            print("You crawl out of the hole and emerge into a large cave with a single bright torch.")
-            print("Beside the torch is a tall figure in a black cape that covers its entire body.")
-            print("Its decaying hand outstrecthes to yours.")
-            print(name + ", I seeeeee rightttt throuuuuugh yooooou. Yooooou havvvvve commmmmeeee herrrrre lookinnnnnnng forrrr answersssss.'")
-            print("'I haaaaave whatttttt yoooooou seeeeeeek.'")
-            print("Before you appears two swords.")
-            print("'Withhhhhhh thissssss youuuuu cannnnnnn takeeeeee baccccck whatttttt isssss yoursssss. Yourrrrrr faaaaaaaather.'")
-            youWentRight(name)
-            print("")
+    if door == "turn back":
+        print("")
+        print("You decide to turn around and not see what craziness awaits you on the other side of the door")
+        print("You make your way back up the fork in the cave and go down the right side instead.")
+        print("A smaller cave is no problem to you. Could be better than the creepy door. You decide to climb into the smaller hold head first.")
+        print("You inch forward every so slowly. It feels like forever, but you reach the source of the dim light.")
+        print("You crawl out of the hole and emerge into a large cave with a single bright torch.")
+        print("Beside the torch is a tall figure in a black cape that covers its entire body.")
+        print("Its decaying hand outstrecthes to yours.")
+        print(name + ", I seeeeee rightttt throuuuuugh yooooou. Yooooou havvvvve commmmmeeee herrrrre lookinnnnnnng forrrr answersssss.'")
+        print("'I haaaaave whatttttt yoooooou seeeeeeek.'")
+        print("Before you appears two swords.")
+        print("'Withhhhhhh thissssss youuuuu cannnnnnn takeeeeee baccccck whatttttt isssss yoursssss. Yourrrrrr faaaaaaaather.'")
+        youWentRight(name)
+        print("")
 
 def youWentRight(name):
+    sword = ""
+    while sword != "left" and sword != "right":
+        sword = raw_input("Do you take the crimson sword on the left or the pitch black one on the right? left/right \n").lower().strip()
+
+    if sword == "left":
+        print("")
+        print("You pick the crimson sword.")
+        print("'Many mennnnn havvvvvve mettttt theeeeeir endddddd byyyyyy thissssss sworrrrrrrd. A Great Sacrifice isssssss requirrrrrrred. Goodddddddd lucccccccck.'")
+        print("The creature disappears and you are surrounded by a blinding light.")
+        print("You find yourself in a lush field near your house.")
+        print("You turn around to thank the creature but the cave is gone. You can only see the forest that is a few hundred yards out.")
+        print("You head in the direction of where the bandits were going.")
+        print("You reach their camp by nightfall and take shelter up in a tree further away from where they've stationed themselves.")
+        midnightStrike2(name)
+        print("")
+    
+    if sword == "right":
+        print("")
+        print("You pick the pitch black sword.")
+        print("'Inttttttteressssting. I nowwwww sssseeeeee yourrrrr Heart. Goooodddddddd lucccccccck.'")
+        print("The creature disappears and you are surrounded by a blinding light.")
+        print("You find yourself in a lush field near your house.")
+        print("You turn around to thank the creature but the cave is gone. You can only see the forest that is a few hundred yards out.")
+        print("You head in the direction of where the bandits were going.")
+        print("You reach their camp by nightfall and take shelter up in a tree further away from where they've stationed themselves.")
+        print("At midnight the pitch black sword begins to glow a deep purple.")
+        print("The sword speaks...")
+        print("'TRAVELER...I SEE YOUR HEART. I SEE YOUR GREATEST WISH. I SEE A GREAT DISTURBANCE. A WORLD OF VIOLENCE.'")
+        print("'NOW, I WILL TAKE WHAT IS MINE.'")
+        print("The sword fuses with your arm and you scream in agony.")
+        print("'A TIME OF GREAT CLEANSING IS UPON US.'")
+        print("Many purple lights shoot forth from the sword and head towards the camp. The screams of soldiers are heard.")
+        print("You run towards the camp to find the soldiers engulfed in dark flames.")
+        print("You make your way towards the larger tent where the leader is stationed and enter it.")
+        print("'YOU. WHAT DID YOU DO? Demon, have you come for me as well?'")
+        print("Your arm feels as though it is on fire. You notice a sword in the hands of a felled soldier.")
+        midnightStrike3(name)
+        print("")
+
+
+def midnightStrike2(name):
+    strike2 = ""
+    while strike2 != "cut it off" and strike2 != "kill him":
+        strike2 = raw_input("Do you cut off your arm to stop the pain or kill the leader now? \n cut it off/kill him").lower().strip()
+
+    if strike2 == "cut it off":
+        print("")
+        print("You reach for the sword in the hands of the felled soldier and cut off your cursed arm.")
+        print("The sword breaks into a hundred pieces. You could not break the skin.")
+        print("'TRY TO KILL ME, WILL YOU?'")
+        print("Your fused arm pulsates and it hurts like hell. A black tentacle emerges from the center of your chest and pierces Solomon, cutting him in two.")
+        print("")
 
 
 def witchRiddle(name):
@@ -421,58 +478,58 @@ def witchRiddle(name):
     while answer != "red" and answer != "white" and answer != "blue" and answer != "all three":
         answer = raw_input("What are the colors of the American flag? red/white/blue/all three \n").lower().strip()
 
-            if answer == "red":
-                print("")
-                print("'HAHAHAHA NOT.'")
-                print("The witch casts a fire spell and you burst into flames.")
-                print("The witch produces a single shrimp and attaches it to a long stick. She holds it out and uses your burning body to cook the shrimp.")
-                playAgain(name)
-                print("")
+    if answer == "red":
+        print("")
+        print("'HAHAHAHA NOT.'")
+        print("The witch casts a fire spell and you burst into flames.")
+        print("The witch produces a single shrimp and attaches it to a long stick. She holds it out and uses your burning body to cook the shrimp.")
+        playAgain(name)
+        print("")
 
-            if answer == "white":
-                print("")
-                print("'WRONG.'")
-                print("The witch picks up a trident and throws it right into your chest. You bleed out and die the most painful death.")
-                print("Foolish boy.")
-                playAgain(name)
-                print("")
+    if answer == "white":
+        print("")
+        print("'WRONG.'")
+        print("The witch picks up a trident and throws it right into your chest. You bleed out and die the most painful death.")
+        print("Foolish boy.")
+        playAgain(name)
+        print("")
 
-            if answer == "blue":
-                print("")
-                print("'PFFFFT AS IF.'")
-                print("The witch draws a sword and cleaves off your head. She holds it in front of your body as it collapses.")
-                print("The last thing you heard is the witch's intense cackling.")
-                playAgain(name)
-                print("")
+    if answer == "blue":
+        print("")
+        print("'PFFFFT AS IF.'")
+        print("The witch draws a sword and cleaves off your head. She holds it in front of your body as it collapses.")
+        print("The last thing you heard is the witch's intense cackling.")
+        playAgain(name)
+        print("")
 
-            if answer == "all three":
-                print("")
-                print("'HAHAHA WRONG.'")
-                print("Oh wait. No. You're right...")
-                print("'CORRECT.'")
-                print("You have passed. The witch hands you the Kingslayer; a sword that has made kingdoms crumble.")
-                print("Why she gave you the sword after a single physics question, I'm not sure. But I'd take it.")
-                print("Ther witch leads you through a door in the back of the room and you walk out onto a lush field near your house.")
-                print("You turn around to thank the witch but the cave is gone. You can only see the forest that is a few hundred yards out.")
-                print("You head in the direction of where the bandits were going.")
-                print("You reach their camp by nightfall and take shelter up in a tree further away from where they've stationed themselves.")
-                midnightStrike(name)
-                print("")
+    if answer == "all three":
+        print("")
+        print("'HAHAHA WRONG.'")
+        print("Oh wait. No. You're right...")
+        print("'CORRECT.'")
+        print("You have passed. The witch hands you the Kingslayer; a sword that has made kingdoms crumble.")
+        print("Why she gave you the sword after a single physics question, I'm not sure. But I'd take it.")
+        print("Ther witch leads you through a door in the back of the room and you walk out onto a lush field near your house.")
+        print("You turn around to thank the witch but the cave is gone. You can only see the forest that is a few hundred yards out.")
+        print("You head in the direction of where the bandits were going.")
+        print("You reach their camp by nightfall and take shelter up in a tree further away from where they've stationed themselves.")
+        midnightStrike(name)
+        print("")
 
 def midnightStrike(name):
     attack = ""
     while attack != "attack now" and attack != "wait":
         attack = raw_input("Do you attack now or wait until the following day? \n").lower().strip()
 
-            if attack == "attack now":
-                print("")
-                print("You do something")
-                print("")
+    if attack == "attack now":
+        print("")
+        print("You do something")
+        print("")
 
-            if attack == "wait":
-                print("")
-                print("You do something")
-                print("")
+    if attack == "wait":
+        print("")
+        print("You do something")
+        print("")
     
 
 
